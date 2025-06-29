dd08753e4e323591616eb41da940a4b01471ec9a
vishal <vp151397@gmail.com>
2025-06-29 22:01:31 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount]=useState(0)
+  const handleInput=(sign)=>{
+    setCount((previousCount)=>(sign==='+'?previousCount+1:previousCount-1))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Counter App</h1>
+    <div>
+      <h2>Count : {count}</h2>
+      <button onClick={()=>(handleInput('+'))}>Increment</button>
+      <button onClick={()=>(handleInput('-'))}>Decrement</button>
     </div>
+    </>
   );
 }
 
